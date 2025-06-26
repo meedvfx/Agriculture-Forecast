7d34a4ec262e5247e503d38fcdcd7e6ba5e7531f
meedvfx <mohamedzahzouh2006@gmail.com>
2025-06-26 22:24:20 +0100

    Create modele.py

---
+++ modele/modele.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder, MinMaxScaler, MaxAbsScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.pipeline import make_pipeline, Pipeline
+from xgboost import XGBRegressor
+from sklearn.compose import ColumnTransformer
+from catboost import CatBoostRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.model_selection import RandomizedSearchCV
+from scipy.stats import randint, uniform
+import numpy as np
+from sklearn.model_selection import GridSearchCV
+from skopt import BayesSearchCV
+from skopt.space import Real, Integer, Categorical
+from itertools import product
+from datetime import date
+from dateutil.relativedelta import relativedelta
+import locale
+from dateutil import parser
+from sklearn.ensemble import HistGradientBoostingRegressor
+from sklearn.experimental import enable_halving_search_cv
+from sklearn.model_selection import HalvingRandomSearchCV
+from scipy.stats import randint, uniform
