db3d07729b04075ca240e9082eded688150f30ba
meedvfx <mohamedzahzouh2006@gmail.com>
2025-06-27 12:36:15 +0100

    Update Prédiction de Prix.py

--- pages/Prédiction de Prix.py
+++ pages/Prédiction de Prix.py
@@ -7,15 +7,6 @@ Created on Thu Jun 26 09:26:15 2025
 
 import streamlit as st
 import pandas as pd
-import pickle  # pour charger le modèle déjà entraîné si nécessaire
-
-##########################################################################3
-st.set_page_config(page_title="Prédiction de Prix des Produits", layout="centered", page_icon="data:image/svg+xml,%3csvg stroke-width='1.75' id='Layer_1' data-name='Layer 1' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3e%3cdefs%3e%3cstyle%3e.cls-n1wrucdvdcsstajotcwn1-1%7bfill:none%3bstroke:%23FC0F0F%3bstroke-miterlimit:10%3b%3b%7d%3c/style%3e%3c/defs%3e%3cpolyline class='cls-n1wrucdvdcsstajotcwn1-1' points='7.23 6.27 1.5 12 7.23 17.73'/%3e%3cpolyline class='cls-n1wrucdvdcsstajotcwn1-1' points='16.77 17.73 22.5 12 16.77 6.27'/%3e%3cline class='cls-n1wrucdvdcsstajotcwn1-1' x1='11.05' y1='12' x2='12.95' y2='12'/%3e%3cline class='cls-n1wrucdvdcsstajotcwn1-1' x1='15.82' y1='12' x2='17.73' y2='12'/%3e%3cline class='cls-n1wrucdvdcsstajotcwn1-1' x1='6.27' y1='12' x2='8.18' y2='12'/%3e%3c/svg%3e")
-
-st.title("📊 Prédiction du Prix des Produits")
-st.write("Cette application permet de prédire le prix des produits selon la ville, le produit et la date.")
-
-# ----------------- Chargement du modèle -----------------
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
 from sklearn.ensemble import RandomForestRegressor
@@ -23,6 +14,14 @@ from sklearn.pipeline import make_pipeline, Pipeline
 from sklearn.compose import ColumnTransformer
 import numpy as np
 from datetime import date
+##########################################################################3
+st.set_page_config(page_title="Prédiction de Prix des Produits", layout="centered", page_icon="data:image/svg+xml,%3csvg stroke-width='1.75' id='Layer_1' data-name='Layer 1' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3e%3cdefs%3e%3cstyle%3e.cls-n1wrucdvdcsstajotcwn1-1%7bfill:none%3bstroke:%23FC0F0F%3bstroke-miterlimit:10%3b%3b%7d%3c/style%3e%3c/defs%3e%3cpolyline class='cls-n1wrucdvdcsstajotcwn1-1' points='7.23 6.27 1.5 12 7.23 17.73'/%3e%3cpolyline class='cls-n1wrucdvdcsstajotcwn1-1' points='16.77 17.73 22.5 12 16.77 6.27'/%3e%3cline class='cls-n1wrucdvdcsstajotcwn1-1' x1='11.05' y1='12' x2='12.95' y2='12'/%3e%3cline class='cls-n1wrucdvdcsstajotcwn1-1' x1='15.82' y1='12' x2='17.73' y2='12'/%3e%3cline class='cls-n1wrucdvdcsstajotcwn1-1' x1='6.27' y1='12' x2='8.18' y2='12'/%3e%3c/svg%3e")
+
+st.title("📊 Prédiction du Prix des Produits")
+st.write("Cette application permet de prédire le prix des produits selon la ville, le produit et la date.")
+
+# ----------------- Chargement du modèle -----------------
+
 
 
 @st.cache_data  
