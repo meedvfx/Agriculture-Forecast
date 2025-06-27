a4a4db78f1b3fa019d22493abd86ecdd46120497
meedvfx <mohamedzahzouh2006@gmail.com>
2025-06-27 12:34:47 +0100

    Update Prédiction de Prix.py

--- pages/Prédiction de Prix.py
+++ pages/Prédiction de Prix.py
@@ -16,6 +16,14 @@ st.title("📊 Prédiction du Prix des Produits")
 st.write("Cette application permet de prédire le prix des produits selon la ville, le produit et la date.")
 
 # ----------------- Chargement du modèle -----------------
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.pipeline import make_pipeline, Pipeline
+from sklearn.compose import ColumnTransformer
+import numpy as np
+from datetime import date
+
 
 @st.cache_data  
 def load_data(url):
@@ -24,12 +32,50 @@ def load_data(url):
 
 df = load_data("data/dataset.csv")
 
-@st.cache_resource
-def load_model():
-    with open("modele/model.pkl", "rb") as f:
-        return pickle.load(f)
+df['Date'] = pd.to_datetime(df['Date'])
+df['month'] = df['Date'].dt.month
+df['year'] = df['Date'].dt.year
+df['day'] = df['Date'].dt.day
+df = df.drop(columns=['Date'])
 
-model = load_model()
+Q1 = df["Prix"].quantile(0.25)
+Q3 = df["Prix"].quantile(0.75)
+IQR = Q3 - Q1
+df = df[(df["Prix"] >= (Q1 -1.5 * IQR)) & (df["Prix"] <= (Q1 + 1.5 * IQR))]
+
+x = df.drop('Prix', axis=1)
+y = df['Prix']
+cat_cols = ['Produits', 'Villes']
+num_cols = ['month', 'year', 'day']
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+num_pip = Pipeline(steps = [
+    ('scaler', MinMaxScaler())
+])
+
+cat_pip = Pipeline(steps = [
+    ('encoder', OneHotEncoder())
+])
+col_trans = ColumnTransformer(transformers = [
+    ('num_pipe', num_pip, num_cols),
+    ('cat_pipe', cat_pip, cat_cols),
+])
+
+
+pipe_rf = Pipeline(steps=[
+    ('preprocessor', col_trans),
+    ('regressor', RandomForestRegressor(        
+        n_jobs=-1,
+        random_state=42,
+        warm_start=True,
+        bootstrap = True,
+        max_depth = 500,
+        oob_score = True,
+    ))
+])
+
+pipe_rf.fit(x_train, y_train)
+
+model = pipe_rf
 
 # ----------------- Interface Utilisateur -----------------
 
