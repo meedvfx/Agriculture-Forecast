60e5ac489d30634a5ad6972af075ff5f986adcc2
Mohamed ZAHZOUH <mohamedzahzouh2006@gmail.com>
2025-06-30 09:47:03 +0100

    Update 🌱 Prédire la Production Agricole.py

--- pages/🌱 Prédire la Production Agricole.py
+++ pages/🌱 Prédire la Production Agricole.py
@@ -8,7 +8,7 @@ from datetime import date
 st.set_page_config(
     page_title="Prédiction de la Production Agricole",
     layout="centered",
-    page_icon="🌾"
+    page_icon="data:image/svg+xml,%3csvg stroke-width='1.75' id='Layer_1' data-name='Layer 1' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3e%3cdefs%3e%3cstyle%3e.cls-n1wrucdvdcsstajotcwn1-1%7bfill:none%3bstroke:%23FC0F0F%3bstroke-miterlimit:10%3b%3b%7d%3c/style%3e%3c/defs%3e%3cpolyline class='cls-n1wrucdvdcsstajotcwn1-1' points='7.23 6.27 1.5 12 7.23 17.73'/%3e%3cpolyline class='cls-n1wrucdvdcsstajotcwn1-1' points='16.77 17.73 22.5 12 16.77 6.27'/%3e%3cline class='cls-n1wrucdvdcsstajotcwn1-1' x1='11.05' y1='12' x2='12.95' y2='12'/%3e%3cline class='cls-n1wrucdvdcsstajotcwn1-1' x1='15.82' y1='12' x2='17.73' y2='12'/%3e%3cline class='cls-n1wrucdvdcsstajotcwn1-1' x1='6.27' y1='12' x2='8.18' y2='12'/%3e%3c/svg%3e"
 )
 
 st.title("🌿 Prédiction de la Production Agricole (en Tonnes)")
